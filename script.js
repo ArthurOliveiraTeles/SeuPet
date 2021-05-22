c738d82f65eaea0c862b13bc3c2f933386184547
ArthurOliveiraTeles <arthur8teles@gmail.com>
2021-05-22 04:36:26 -0300

    Arrumei o menu
    Consegui finalmente arrumar meu menu de opções

--- script.js
+++ script.js
@@ -27,6 +27,14 @@ function Preencher(texto, testa) {
         else if(texto == "subItem2Cachorro") {
             subItem2Cachorro.style.backgroundColor="#2f9b7aca"
         }
+
+        else if(texto == "subItemGato") {
+            subItemGato.style.backgroundColor="#2f9b7aca"
+        }
+
+        else if(texto == "subItem2Gato") {
+            subItem2Gato.style.backgroundColor="#2f9b7aca"
+        }
     }
 
 
@@ -38,18 +46,45 @@ function Preencher(texto, testa) {
         sobre.style.backgroundColor="#561D25"
         subItemCachorro.style.backgroundColor="#66ddb9bb"
         subItem2Cachorro.style.backgroundColor="#66ddb9bb"
+        subItemGato.style.backgroundColor="#66ddb9bb"
+        subItem2Gato.style.backgroundColor="#66ddb9bb"
     }
 }
 
-function MostraSub(testa, entraNoSub=false) { // Preciso indicar depois nessa função qual área esta sendo acessada, por enquanto só do cachorro.
+function MostraSub(texto=false, testa) { // Preciso indicar depois nessa função qual área esta sendo acessada, por enquanto só do cachorro.
     // Como padrão o entraNoSub é false
-    var subItem = document.getElementById("sub-cachorro")
+    
+    if(texto == "subCachorro") {
+        // Removendo o outro sub-menu para o usuário não interagir
+
+        subCachorro.classList.remove('NoClick')
+        var subItem = document.getElementById('subCachorro')
+        subGato.classList.add('NoClick')
+        subGato.style.opacity=0
+
+        } else if(texto == 'subGato') {
+
+        subGato.classList.remove('NoClick')
+        var subItem = document.getElementById('subGato')
+        subCachorro.style.opacity=0
+        subCachorro.classList.add('NoClick')
+
+    }
+    
+
+    if(menu2.style.display == "block") {
+        // Se tiver ativo, ele apaga a área dos sub-menus
+        menu2.style.display="none"
+    }
     if (testa == true) {
+        menu2.style.display="block"
         // Entrei na seção de subitem
         subItem.style.opacity=1
+        subItem.style.transition= 'opacity 0.5s'
         subItem.style.display="block"
     }
 
+    /*
     else if (testa == false) {
         if(entraNoSub == true) {
             // Entrei no meu subitem
@@ -59,8 +94,9 @@ function MostraSub(testa, entraNoSub=false) { // Preciso indicar depois nessa fu
         else if(entraNoSub == false) {
             // Aqui eu estou saindo do meu sub item
             subItem.style.opacity=0
-            subItem.style.display="none"
+            menu2.style.display="none"
         }
-    }
+    }*/
+    
     
 }
